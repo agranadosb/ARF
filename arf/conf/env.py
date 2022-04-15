ba0a61796a4794ef33ee301b2bcff126648dd2bd
Alejandro <agranadosb55199@outlook.com>
2022-04-15 13:55:47 +0200

    feat(env): add documentation and change False per []

--- arf/conf/env.py
+++ arf/conf/env.py
@@ -8,12 +8,34 @@ from arf.constants import RESNET_BLOCKS_ENV_VAR
 
 
 def parse_blocks(blocks: Union[str, IO]) -> List[Tuple[int, int, int]]:
+    """This function gets a yml string like or a yml file representing the list
+    of resnet blocks and return the parsed blocks. The definition of this file
+    should be like this:
+
+    ```yaml
+    blocks:
+      -
+        - 1     # repetitions of this block
+        - 3     # kernel_size
+        - 64    # out_channels
+      -
+        - 1     # repetitions of this block
+        - 3     # kernel_size
+        - 128   # out_channels
+    ```
+    
+    Args:
+        blocks: A yml string or a yml file representing the list of resnet blocks.
+
+    Returns:
+        A list of tuples representing the blocks.
+    """
     parsed_yaml = yaml.safe_load(blocks)
     
     if not isinstance(parsed_yaml, dict):
         raise ValueError("Invalid YAML")
     
-    block_list = parsed_yaml.get("blocks", False)
+    block_list = parsed_yaml.get("blocks", [])
     
     if not block_list:
         raise ValueError("No blocks found in config file")
