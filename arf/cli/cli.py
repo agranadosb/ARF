8b56b886079b2d25a82af3cb7dec73b2c64fd24c
Alejandro <agranadosb55199@outlook.com>
2022-04-15 13:47:38 +0200

    feat(cli): add todo

---
+++ arf/cli/cli.py
@@ -0,0 +1,11 @@
+""" TODO: Create a CLI for the ARF project. This cli should be able to:
+ - Show the enviorment variables and its descriptions (from constants).
+ - Show the ARF project version (from root __init__).
+ - Launch training experiments (for this three env variables should be defined:
+    Training, validation and test). The model that will be trained will given
+    by the user (resnet, inception or transformer).
+ - Show the help menu (created by argparse).
+ - Show statistics about the trainings and validations (to define which charts).
+
+All the functions created for
+"""
