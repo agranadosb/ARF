6cb96b367fd7e4a8d5849e476dc37ba5bc4f6b0b
Alejandro <agranadosb55199@outlook.com>
2022-04-19 16:12:34 +0200

    feat(constants): add constants about data to constants index. The INDEX_LABEL will be added too.

--- arf/constants/__init__.py
+++ arf/constants/__init__.py
@@ -2,11 +2,16 @@ from arf.constants.constants import *
 
 __all__ = [
     'ENV_VARIABLES',
+    'INDEX_LABEL',
     'LABEL_TYPE_OPTIONS',
     'ONE_HOT_LABEL',
     'PAIR_TYPE',
+    'RESNET_BATCH_SIZE_VAR',
     'RESNET_BLOCKS_ENV_VAR',
+    'TEST_DATA_VAR',
     'TEXT_LABEL',
+    'TRAINING_DATA_VAR',
+    'VALIDATION_DATA_VAR',
 ]
 
 assert sorted(__all__) == __all__
